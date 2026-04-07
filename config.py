feeb7f74c54466d64611f10b2759aa27aa2acbf3
SarahC <46381735+sarcho22@users.noreply.github.com>
2026-04-07 12:46:22 -0400

    trying to fix key....

--- config.py
+++ config.py
@@ -30,7 +30,5 @@ LOG_DIR = os.path.join(BASE_DIR, "logs")
 SECURITY_LOG = os.path.join(LOG_DIR, "security.log")
 ACCESS_LOG = os.path.join(LOG_DIR, "access.log")
 
-# Basic app settings 
-DEBUG = True
 # "dev-secret-key" is default, can locally run 'export SECRET_KEY="secret"' before running app.py
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
